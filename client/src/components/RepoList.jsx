4ce4bb410f809835ebb7c93fcd1ec7a621b70fa9
alexanderwiederin <alexander.wiederin@gmail.com>
2018-10-19 21:18:43 -0700

    changed keys on repo components

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const RepoList = (props) => (
   <div>
     {props.repos.map((repo) => {
-      return <div key={repo.id}>Repo name: {repo.name} forks: {repo.forks}</div>
+      return <div key={repo._id}>Repo name: {repo.name} forks: {repo.forks}</div>
     })}
   </div>
 )
